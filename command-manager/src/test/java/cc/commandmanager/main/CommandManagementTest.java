3961a574b16b8e2e1aca67620230ce8a44bd4d60
Benjamin Schandera <4.32@gmx.de>
2014-03-07 11:22:11 +0000

    Initial commit of class files and folders

---
+++ command-manager/src/test/java/cc/commandmanager/main/CommandManagementTest.java
@@ -0,0 +1,24 @@
+package cc.commandmanager.main;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class CommandManagementTest {
+
+    private final ChainManagement _chainManager = new ChainManagement();
+
+    @Test
+    public void testSetCatalog() {
+	this._chainManager.setCatalog("/DummyCatalog1.xml");
+	assertThat(this._chainManager.catalog).isNotNull();
+    }
+
+    @Test(expected = CatalogNotInstantiableException.class)
+    public void testSetCatalogWithNoValidCatalog() throws Exception {
+	this._chainManager.setCatalog("");
+	fail();
+    }
+
+}
---
+++ command-manager/src/test/java/cc/commandmanager/main/DummyCommand1.java
@@ -0,0 +1,11 @@
+package cc.commandmanager.main;
+
+import org.apache.commons.chain.Context;
+
+public final class DummyCommand1 extends DependencyCommand {
+
+    @Override
+    public void specialExecute(Context context) {
+	System.err.println("DummyCommad1 was called.");
+    }
+}
