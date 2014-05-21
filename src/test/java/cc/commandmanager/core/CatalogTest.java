a07679330ef4c5d2b727a4601631200f0c40616f
husterknupp <4.23@gmx.de>
2014-05-21 11:22:34 +0200

    #15 support catalog test with a dummy document

---
+++ src/test/java/cc/commandmanager/core/CatalogTest.java
@@ -0,0 +1,52 @@
+package cc.commandmanager.core;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class CatalogTest {
+
+	Catalog catalog = null;
+
+	@Before
+	public void setup() throws ParserConfigurationException {
+		catalog = Catalog.fromDomDocument(createCatalogDocument());
+	}
+
+	private static Document createCatalogDocument() throws ParserConfigurationException {
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		Document catalogDocument = dBuilder.newDocument();
+
+		Element catalog = catalogDocument.createElement("catalog");
+
+		Element command1 = catalogDocument.createElement("command");
+		command1.setAttribute("className", "cc.commandmanager.core.Command1");
+		command1.setAttribute("name", "Command1");
+		catalog.appendChild(command1);
+
+		Element command2 = catalogDocument.createElement("command");
+		command2.setAttribute("className", "cc.commandmanager.core.Command2");
+		command2.setAttribute("name", "Command2");
+		catalog.appendChild(command2);
+
+		catalogDocument.appendChild(catalog);
+		return catalogDocument;
+	}
+
+	@Test
+	public void testGetNames() {
+		// TODO implement test case
+	}
+
+	@Test
+	public void testGetCommand() {
+		// TODO implement test case
+	}
+
+}
