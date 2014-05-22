332569028e0990aeb51e4d1da171575f365bef61
husterknupp <4.23@gmx.de>
2014-05-22 10:12:09 +0200

    #15 rework test

--- src/test/java/cc/commandmanager/core/CatalogTest.java
+++ src/test/java/cc/commandmanager/core/CatalogTest.java
@@ -6,7 +6,6 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
-import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.w3c.dom.Document;
@@ -15,39 +14,40 @@ import org.w3c.dom.Element;
 public class CatalogTest {
 
 	Catalog catalog = null;
+	Document catalogDocument = createBaseCatalogDocument();
 
-	@Before
-	public void setup() throws ParserConfigurationException {
-		catalog = Catalog.fromDomDocument(createCatalogDocument());
-	}
-
-	private static Document createCatalogDocument() throws ParserConfigurationException {
-		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-		Document catalogDocument = dBuilder.newDocument();
-
-		Element catalog = catalogDocument.createElement("catalog");
+	@Test
+	public void testGetNames() throws ParserConfigurationException {
+		Element documentRoot = catalogDocument.createElement("catalog");
 
 		Element command1 = catalogDocument.createElement("command");
 		command1.setAttribute("className", "cc.commandmanager.core.Command1");
 		command1.setAttribute("name", "Command1");
-		catalog.appendChild(command1);
+		documentRoot.appendChild(command1);
 
 		Element command2 = catalogDocument.createElement("command");
 		command2.setAttribute("className", "cc.commandmanager.core.Command2");
 		command2.setAttribute("name", "Command2");
-		catalog.appendChild(command2);
+		documentRoot.appendChild(command2);
 
-		catalogDocument.appendChild(catalog);
-		return catalogDocument;
-	}
+		catalogDocument.appendChild(documentRoot);
+		catalog = Catalog.fromDomDocument(catalogDocument);
 
-	@Test
-	public void testGetNames() {
 		assertThat(catalog.getCommandNames()).containsOnly("Command1", "Command2");
 	}
 
-	@Ignore
+	private static Document createBaseCatalogDocument() {
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder;
+		try {
+			dBuilder = dbFactory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			throw new RuntimeException(e);
+		}
+		Document catalogDocument = dBuilder.newDocument();
+		return catalogDocument;
+	}
+
 	@Test(expected = MissingDomAttributeException.class)
 	public void testGetNamesThrowsException() {
 		// TODO implement test case
