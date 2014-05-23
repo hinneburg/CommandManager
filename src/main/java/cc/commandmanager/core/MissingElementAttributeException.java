c95cc64814d94524a4f8ef5b4902f5da65b7ccba
husterknupp <4.23@gmx.de>
2014-05-23 12:13:05 +0200

    #15 debug (count attribute position properly)

--- src/main/java/cc/commandmanager/core/MissingElementAttributeException.java
+++ src/main/java/cc/commandmanager/core/MissingElementAttributeException.java
@@ -1,6 +1,5 @@
 package cc.commandmanager.core;
 
-
 public final class MissingElementAttributeException extends RuntimeException {
 
 	private static final long serialVersionUID = -8375366289082678959L;
@@ -8,6 +7,6 @@ public final class MissingElementAttributeException extends RuntimeException {
 	public MissingElementAttributeException(String documentUri, String elementName, int attributePosition,
 			String attributeName) {
 		super(String.format("Missing attribute %s in DOM document %s. Missing at the &d. element with tag %s node",
-				attributeName, documentUri, attributePosition + 1, elementName));
+				attributeName, documentUri, attributePosition, elementName));
 	}
 }
