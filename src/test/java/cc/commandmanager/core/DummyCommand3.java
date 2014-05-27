12f9096720fdff9995b3a0bd049ae0ff7aaa2317
husterknupp <4.23@gmx.de>
2014-05-27 20:23:47 +0200

    document intention of dummy commands

--- src/test/java/cc/commandmanager/core/DummyCommand3.java
+++ src/test/java/cc/commandmanager/core/DummyCommand3.java
@@ -6,8 +6,16 @@ import java.util.Set;
 
 import com.google.common.collect.Sets;
 
+/**
+ * Dummy {@link Command} implementation with a before dependency on {@link DummyCommand1} and an optional before
+ * dependency on {@link DummyCommand2}. {@link #execute(Context)} will bind a {@link Class} object corresponding to this
+ * class to the context. A {@link ClassCastException} will be thrown if the context does not have a {@link List<Class<?
+ * extends Command>>}. The list should be bound to the context with the key, specified in the
+ * {@link CommandManagementIntegrationTest}.
+ */
 public final class DummyCommand3 implements Command {
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public void execute(Context context) {
 		((List<Class<? extends Command>>) context.get(CommandManagementIntegrationTest.EXECUTED_COMMANDS)).add(this
--- src/test/java/cc/commandmanager/core/DummyCommand5.java
+++ src/test/java/cc/commandmanager/core/DummyCommand5.java
@@ -4,8 +4,15 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * Dummy {@link Command} implementation with no dependencies. {@link #execute(Context)} will bind a {@link Class} object
+ * corresponding to this class to the context. A {@link ClassCastException} will be thrown if the context does not have
+ * a {@link List<Class<? extends Command>>}. The list should be bound to the context with the key, specified in the
+ * {@link CommandManagementIntegrationTest}.
+ */
 public final class DummyCommand5 implements Command {
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public void execute(Context context) {
 		((List<Class<? extends Command>>) context.get(CommandManagementIntegrationTest.EXECUTED_COMMANDS)).add(this
