7f6b2373e5b71d4dea10c3b760b2bb986c885ed4
Frank Rosner <frank.rosner@student.uni-halle.de>
2014-06-23 23:03:51 +0200

    #36 add JavaDoc to DependencyAdded result states

--- src/main/java/cc/commandmanager/core/DependencyAdded.java
+++ src/main/java/cc/commandmanager/core/DependencyAdded.java
@@ -2,15 +2,58 @@ package cc.commandmanager.core;
 
 import java.util.Set;
 
+import cc.commandmanager.core.CommandGraph.CommandGraphBuilder;
+
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
 
+/**
+ * Result state indicating whether a dependency was added successfully to a {@linkplain CommandGraphBuilder}.
+ */
 public enum DependencyAdded {
 
-	SUCCESSFUL, OPTIONAL_OVERWRITTEN, MANDATORY_NOT_OVERWRITTEN, CYCLE_DETECTED, ALREADY_PRESENT, COMMAND_MISSING;
+	/**
+	 * Dependency added successfully.
+	 */
+	SUCCESSFUL,
 
+	/**
+	 * Mandatory dependency added and overwrote an existing optional one.
+	 */
+	OPTIONAL_OVERWRITTEN,
+
+	/**
+	 * Tried to add an optional dependency where a mandatory one was already present.
+	 */
+	MANDATORY_NOT_OVERWRITTEN,
+
+	/**
+	 * Tried to add a dependency that would have introduced a cycle.
+	 */
+	CYCLE_DETECTED,
+
+	/**
+	 * Tried to add a dependency that is already present. Nothing happened.
+	 */
+	ALREADY_PRESENT,
+
+	/**
+	 * Tried to add a dependency between commands of which one or both do not exist in the
+	 * {@linkplain CommandGraphBuilder}.
+	 */
+	COMMAND_MISSING;
+
+	/**
+	 * {@linkplain Set} of states indicating that adding the dependency did work or at least was not canceled because it
+	 * would do any harm.
+	 */
 	public static Set<DependencyAdded> SUCCESS_STATES = ImmutableSet.of(SUCCESSFUL, OPTIONAL_OVERWRITTEN,
 			ALREADY_PRESENT);
+
+	/**
+	 * {@linkplain Set} of states indicating that adding the dependency was not successful because it violated the
+	 * contract of a {@linkplain CommandGraph}.
+	 */
 	public static Set<DependencyAdded> FAILURE_STATES = Sets.difference(ImmutableSet.copyOf(DependencyAdded.values()),
 			SUCCESS_STATES).immutableCopy();
 
