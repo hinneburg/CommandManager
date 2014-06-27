d191128935f618f09dd97126625f3dddd3dc4bc9
Husterknupp <4.23@gmx.de>
2014-06-27 10:44:49 +0200

    #37 cover method that will be reworked with test case

--- src/test/java/cc/commandmanager/core/CommandManagerTest.java
+++ src/test/java/cc/commandmanager/core/CommandManagerTest.java
@@ -2,7 +2,9 @@ package cc.commandmanager.core;
 
 import static org.fest.assertions.Assertions.assertThat;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -11,10 +13,11 @@ import cc.commandmanager.catalog.Catalog;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 
 public class CommandManagerTest {
 
-	private CommandManager commandManagement;
+	private CommandManager commandManager;
 
 	@Before
 	public void setUpCommandManagement() {
@@ -23,13 +26,42 @@ public class CommandManagerTest {
 		commands.put("Warning", WarningCommand.class);
 		commands.put("Failure", FailingCommand.class);
 		Catalog catalog = Catalog.fromMap(commands);
-		commandManagement = new CommandManager(catalog);
+		commandManager = new CommandManager(catalog);
+	}
+
+	@Test
+	public void testGetDependencies() {
+
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void testDependencies_NPE() {
+		commandManager.getDependencies();
+	}
+
+	@Test
+	public void testGetOrderedCommandsFromDependenciesAndStartCommands() {
+
+	}
+
+	@Test
+	public void testGetOrderedCommandsFromStartAndEndCommands() {
+		Map<String, Set<String>> expected = Maps.newHashMap();
+		expected.put("Success", new HashSet<String>());
+		expected.put("Warning", new HashSet<String>());
+		expected.put("Failure", new HashSet<String>());
+		assertThat(
+				commandManager.getOrderedCommands(Sets.newHashSet("Success", "Warning", "Failure"),
+						new HashSet<String>())).containsOnly("Success", "Warning", "Failure").doesNotHaveDuplicates();
+
+		assertThat(commandManager.getOrderedCommands(Sets.newHashSet("Success"), new HashSet<String>()))
+				.containsExactly("Success");
 	}
 
 	@Test
 	public void testExecutionAbortionOnFailure() {
 		// TODO only way of doing this until #41 is implemented and returns the result state
-		commandManagement.executeCommands(ImmutableList.of("Success", "Warning", "Failure"));
+		commandManager.executeCommands(ImmutableList.of("Success", "Warning", "Failure"));
 		assertThat(SuccessfulCommand.isExecuted).isTrue();
 		assertThat(WarningCommand.isExecuted).isTrue();
 		assertThat(FailingCommand.isExecuted).isTrue();
@@ -37,7 +69,7 @@ public class CommandManagerTest {
 		WarningCommand.isExecuted = false;
 		FailingCommand.isExecuted = false;
 
-		commandManagement.executeCommands(ImmutableList.of("Success", "Failure", "Warning"));
+		commandManager.executeCommands(ImmutableList.of("Success", "Failure", "Warning"));
 		assertThat(SuccessfulCommand.isExecuted).isTrue();
 		assertThat(FailingCommand.isExecuted).isTrue();
 		assertThat(WarningCommand.isExecuted).isFalse();
