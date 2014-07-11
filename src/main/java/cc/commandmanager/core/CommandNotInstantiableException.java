71bd711f3f8e3b800b0997b93e259dd21716d184
Husterknupp <4.23@gmx.de>
2014-07-12 00:02:03 +0200

    #41 minor refactoring (add detail to exception message)

--- src/main/java/cc/commandmanager/core/CommandNotInstantiableException.java
+++ src/main/java/cc/commandmanager/core/CommandNotInstantiableException.java
@@ -5,7 +5,7 @@ public class CommandNotInstantiableException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
 	public CommandNotInstantiableException(String commandClassName, Throwable throwable) {
-		super("Class " + commandClassName + " could not be instantiated: " + throwable.getMessage(), throwable);
+		super("Command class " + commandClassName + " could not be instantiated: " + throwable.getMessage(), throwable);
 	}
 
 }
