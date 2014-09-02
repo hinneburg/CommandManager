55a90894c0bae227fbca87d508d98f93d35b4ea6
Frank Rosner <frank.rosner@student.uni-halle.de>
2014-09-02 15:56:12 +0200

    #30 remove unnecessary package information

--- src/main/java/cc/commandmanager/core/Command.java
+++ src/main/java/cc/commandmanager/core/Command.java
@@ -22,31 +22,27 @@ public interface Command {
 	ResultState execute(Context context);
 
 	/**
-	 * @return names of {@linkplain cc.commandmanager.core.Command}s that need to be executed before this
-	 *         {@linkplain cc.commandmanager.core.Command} can be executed.
+	 * @return names of {@linkplain Command}s that need to be executed before this {@linkplain Command} can be executed.
 	 */
 	Set<String> getBeforeDependencies();
 
 	/**
-	 * @return names of {@linkplain cc.commandmanager.core.Command}s can be executed after this
-	 *         {@linkplain cc.commandmanager.core.Command} was executed.
+	 * @return names of {@linkplain Command}s can be executed after this {@linkplain Command} was executed.
 	 */
 	Set<String> getAfterDependencies();
 
 	// TODO change this description when optional dependency behavior changes. See #53.
 	/**
-	 * @return names of {@linkplain cc.commandmanager.core.Command}s that need to be executed before this
-	 *         {@linkplain cc.commandmanager.core.Command} can be executed. Optional means that building a
-	 *         {@linkplain cc.commandmanager.core.CommandGraph} does not fail when a dependency that is returned by this
-	 *         method cannot be found in the catalog. In future releases optional behavior will be changed.
+	 * @return names of {@linkplain Command}s that need to be executed before this {@linkplain Command} can be executed.
+	 *         Optional means that building a {@linkplain CommandGraph} does not fail when a dependency that is returned
+	 *         by this method cannot be found in the catalog. In future releases optional behavior will be changed.
 	 */
 	Set<String> getOptionalBeforeDependencies();
 
 	// TODO change this description when optional dependency behavior changes. See #53.
 	/**
-	 * @return names of {@linkplain cc.commandmanager.core.Command}s can be executed after this
-	 *         {@linkplain cc.commandmanager.core.Command} was executed. Optional means that building a
-	 *         {@linkplain cc.commandmanager.core.CommandGraph} does not fail when a dependency that is returned by this
+	 * @return names of {@linkplain Command}s can be executed after this {@linkplain Command} was executed. Optional
+	 *         means that building a {@linkplain CommandGraph} does not fail when a dependency that is returned by this
 	 *         method cannot be found in the catalog. In future releases optional behavior will be changed.
 	 */
 	Set<String> getOptionalAfterDependencies();
