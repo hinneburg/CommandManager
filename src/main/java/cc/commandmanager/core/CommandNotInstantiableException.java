e7ea3e83c3555236f1beddfd9922f47cbb75d93e
Frank Rosner <frank.rosner@student.uni-halle.de>
2014-09-11 10:37:12 +0200

    #30 JavaDoc CommandNotInstantiableException

--- src/main/java/cc/commandmanager/core/CommandNotInstantiableException.java
+++ src/main/java/cc/commandmanager/core/CommandNotInstantiableException.java
@@ -1,9 +1,22 @@
 package cc.commandmanager.core;
 
+/**
+ * {@linkplain RuntimeException} thrown if a given command cannot be instantiated. It will wrap the exception that is
+ * the reason for the error.
+ */
 public class CommandNotInstantiableException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Creates a new {@linkplain CommandNotInstantiableException} with a custom message containing the command class and
+	 * the original reason for instantiation failure.
+	 * 
+	 * @param commandClassName
+	 *            of the command that cannot be instantiated
+	 * @param throwable
+	 *            that is the cause of the instantiation failure
+	 */
 	public CommandNotInstantiableException(String commandClassName, Throwable throwable) {
 		super("Command class " + commandClassName + " could not be instantiated: " + throwable.getMessage(), throwable);
 	}
