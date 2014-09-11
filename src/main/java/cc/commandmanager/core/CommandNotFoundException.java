378d7448fddbae3a84d418285c75da388ec8f8de
Frank Rosner <frank.rosner@student.uni-halle.de>
2014-09-11 10:29:11 +0200

    #30 JavaDoc CommandNotFoundException

--- src/main/java/cc/commandmanager/core/CommandNotFoundException.java
+++ src/main/java/cc/commandmanager/core/CommandNotFoundException.java
@@ -1,9 +1,19 @@
 package cc.commandmanager.core;
 
+/**
+ * {@linkplain RuntimeException} indicating that a given command expected to be in a {@linkplain CommandGraph} could not
+ * be found.
+ */
 public class CommandNotFoundException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Creates a new {@linkplain CommandNotFoundException} with a custom message containing the given command.
+	 * 
+	 * @param commandName
+	 *            of the missing command
+	 */
 	public CommandNotFoundException(String commandName) {
 		super("Command \"" + commandName + "\" cannot be found in the given command graph.");
 	}
